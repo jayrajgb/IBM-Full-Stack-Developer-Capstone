f0cd6bdaaf64d1b6501dc3f8f9319e18c87295e7
jayrajgb <jayrajgborate11@gmail.com>
2024-10-20 23:22:52 +0530

    module 5 CI/CD v1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,29 +5,25 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name='register'),
-
+    # path for registration
+    path(route='registration', view=views.registration, name='registration'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    
     # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),    
-
+    path(route='logout', view=views.logout, name='logout'),
+    # path for getting cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    # path for dealer list view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    # path for getting dealers by state
+    path(route='get_dealers/<str:state>', view=views.get_dealerships,
+         name='get_dealers_by_state'),
+    # path for getting dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
+         name='dealer_details'),
+    # path for getting dealer reviews
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews,
+         name='dealer_reviews'),
+    # path for adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
+
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -32,6 +31,7 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://jayrajborate-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
+
 CSRF_TRUSTED_ORIGINS = ['https://jayrajborate-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
@@ -39,7 +39,6 @@ REST_FRAMEWORK = {
 }
 
 # Application definition
-
 INSTALLED_APPS = [
     'djangoapp.apps.DjangoappConfig',
     'django.contrib.admin',
@@ -83,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -113,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -126,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -146,4 +142,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
